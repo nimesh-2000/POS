14bdfdb2665e3332dcff381eaca3ff15ca051f88
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2022-05-02 23:24:34 +0530

    delete customer

--- Layered_Architecture/src/dao/CustomerDAOImpl.java
+++ Layered_Architecture/src/dao/CustomerDAOImpl.java
@@ -47,4 +47,11 @@ public class CustomerDAOImpl {
         pstm.setString(1, id);
         return pstm.executeQuery().next();
     }
+    //delete customer
+    public boolean deleteCustomer(String id) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("DELETE FROM Customer WHERE id=?");
+        pstm.setString(1, id);
+        return pstm.executeUpdate() > 0;
+    }
 }
